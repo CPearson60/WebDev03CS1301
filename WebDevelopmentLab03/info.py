af893b92c3247767750f480faabef3c26c9bd9ff
Cameron Pearson <cameronlpearson2005@gmail.com>
2024-11-25 13:52:47 -0500

    Update info.py

--- WebDevelopmentLab03/info.py
+++ WebDevelopmentLab03/info.py
@@ -1,45 +1,52 @@
+# This File will contain the information to be displayed in your portfolio
 
-#This File will contain the information to be displayed in your portfolio
+# CHANGE BELOW
+profile_picture = "Images/Cameron_Suit.jpg"
+about_me = "My name is Cameron Pearson. I am currently a student at the amazing Georgia Tech, located in Atlanta, Georgia."
 
-#CHANGE BELOW
-profile_picture = "Images/Cameron_Casual.jpg"
-about_me = "My name is Cameron Pearson. I am currently a student at the amazing Georgia Tech, located in Atlanta, Georgia. "
-
-
-#CHANGE BELOW (OPTIONAL)
+# CHANGE BELOW (OPTIONAL)
 linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
 github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
 email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
 
-#CHANGE BELOW
+# CHANGE BELOW
 my_linkedin_url = "www.linkedin.com/in/cameron-campearson"
 my_github_url = "https://github.com/CPearson60"
 my_email_address = "CameronLPearson2005@gmail.com"
 
-
-education_data ={
+education_data = {
     'Degree': 'Bachelor of Science in Electrical Engineering',
     'Institution': 'Georgia Institute of Technology',
     'Location': 'Atlanta, GA',
     'Graduation Date': '2028',
     'GPA': 'N/A'
 }
+
 course_data = {
-    "code":["CS 1301", "Math 1551", "Chem 1310"], 
-    "names":["Intro to CS", "Differntial Calculus", "Principles of Gen Chem for Engineers"], 
-    "semester_taken":["1st", "1st", "1st"],
-    "skills":["Python, Html, Swift, Working In Teams, Github"],
-    }
+    "code": ["CS 1301", "Math 1551", "Chem 1310"], 
+    "names": ["Intro to CS", "Differential Calculus", "Principles of Gen Chem for Engineers"], 
+    "semester_taken": ["1st", "1st", "1st"],
+    "skills": [
+        "Python, Html, Swift, Working In Teams, Github", 
+        "Limits, Derivatives, Calculus Concepts", 
+        "Chemical Equations, Stoichiometry, Engineering Applications"
+    ],
+}
+
 experience_data = {
-    "Summer Intern for STEM@Gtri": (["- Worked on designing and creating an IOT Smart Garden",
-                                                                          "- Created Electrical Wiring Schematic ", "- Helped Install Raspi, Mositure Sensors and Electrical Wiring ", "-Intergrated Slack's Api with our Garden Bed To Create A Messaging System Between The Graden and Slack."],"Images/GardenBed.jpg"),
-    "Intern For 402nd SWEG - Robins AFB":(["- Created numerous programs in the python language", "- Worked apart of a team, to create and present a final product" ],"Images/Sweg_Cameron.jpg"),
-
-
+    "Summer Intern for STEM@Gtri": ([
+        "- Worked on designing and creating an IoT Smart Garden",
+        "- Created Electrical Wiring Schematic",
+        "- Helped Install Raspi, Moisture Sensors, and Electrical Wiring",
+        "- Integrated Slack's API with our Garden Bed to create a messaging system between the garden and Slack."
+    ], "Images/GardenBed.jpg"),
+    "Intern For 402nd SWEG - Robins AFB": ([
+        "- Created numerous programs in the Python language"
+    ], "Images/Sweg_Cameron.jpg"),
 }
 
 projects_data = {
-    "Study Buddy (Swift App)": "In the process of creating an app that will serve as a study helper to students, mainly GT Students.",
+    "Study Buddy (Swift App)": "In the process of creating an app that will serve as a study helper to students, mainly GT students.",
 }
 
 programming_data = {
@@ -48,30 +55,35 @@ programming_data = {
     "C": 40,
 }
 
-#CHANGE BELOW (OPTIONAL)
+# CHANGE BELOW (OPTIONAL)
 programming_icons = {
     "Python": "🐍",
     "Swift": "🐦‍🔥",
-    "HTML: "🕸️",
-}
-spoken_icons = {"English": "🇺🇸"
+    "HTML": "🕸️",
 }
 
-#CHANGE BELOW
+spoken_icons = {
+    "English": "\U0001F1FA\U0001F1F8",
+}
+
+# CHANGE BELOW
 spoken_data = {
     "English": "Fluent",
 }
-leadership_data = {
-    "GT-BCIT Mentor": (["- I mentor a highschool who plans to seek out higher education."],"Gatech_Logo"),
 
+leadership_data = {
+    "GT-BCIT Mentor": ([
+        "- I mentor a high school student who plans to seek out higher education."
+    ], "Images/gatech.png"),
 }
-activity_data={
-    "LDR": ["- Attend Meetings", 
-            "- Participate in some events"]
-    "Working Out": ["- Attend the gym atleast 3 times a week",
-                    "- Try to become healthier through physical activity"]
-v   "Playing Piano": ["- I play piano in my free time",
-                      "- I play more of an r&b/neo-soul style "]
-    "Photography": ["- Im trying to get into photography at the moment",
-    				" - On the weekends me and my friend will go out an take random pictures of things"]
+
+activity_data = {
+    "LDR": ([
+        "- Attend Meetings",
+        "- Participate in some events"
+    ]),
+    "Working Out": ([
+        "- Attend the gym at least 3 times a week",
+        "- Try to become healthier through physical activity"
+    ])
 }
